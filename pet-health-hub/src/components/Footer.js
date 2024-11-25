a93e5424589c91189cba4007bae041eb71f7b681
Lavieboheme2018 <janjprvj@gmail.com>
2024-11-24 18:52:42 -0800

    add github link at footer

--- pet-health-hub/src/components/Footer.js
+++ pet-health-hub/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Footer.css';
+import { FaGithub } from 'react-icons/fa';
 
 function Footer() {
   return (
@@ -10,6 +11,17 @@ function Footer() {
         <Link to="/">Home</Link> 
         <Link to="/about">About</Link>
       </nav>
+      <div className="github-link">
+        <a
+          href="https://github.com/Lavieboheme2018/CS5610Web.git"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub Repository"
+        >
+          <FaGithub size={30} style={{ color: '#f5f5f5' }} />
+        </a>
+      </div>
+      <div className="footer-logo"></div>
     </footer>
   );
 }
